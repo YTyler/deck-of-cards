9756d5fe73400b3fb1cc69d4fa0a277415d82871
Student <example@example.com>
2019-10-28 14:49:04 -0700

    create an array containing 4 arrays, one for every suit

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,14 @@
-$(document).ready(function(){
-  var types = ['clubs', 'diamonds', 'spades', 'hearts']
-  var numbers = ['ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'jack', 'queen', 'king']
 
-  types.forEach(function(type){
-    numbers.forEach(function(number){
-    var deck = number + ' of ' + type;
-    console.log(deck)
+$(document).ready(function(){
+  var types = ['clubs', 'diamonds', 'spades', 'hearts'];
+  var numbers = ['ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'jack', 'queen', 'king'];
+
+
+  var deck = types.map(function(type){
+    return numbers.map(function(number){
+      // console.log(number.concat(' of ' + type))
+      return (number + ' of ' + type)
     });
   });
+  console.log(deck);
 });
